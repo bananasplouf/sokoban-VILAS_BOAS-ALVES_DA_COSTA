198681e841a39d5a7bd57a2f99783c9614cc04d2
bananasplouf <paco.vb@protonmail.com>
2024-04-09 21:42:08 +0200

    implementation de la lecture et ecriture de fichier de carte

--- sokoban/src/modele/Game.java
+++ sokoban/src/modele/Game.java
@@ -83,89 +83,198 @@ public class Game extends Observable
         Block b = new Block(this, entityGrid[6][6]);
     }
 
-    private void addTile(Tile e, int x, int y)
-    {
+    private void addTile(Tile e, int x, int y) {
         entityGrid[x][y] = e;
         map.put(e, new Point(x, y));
     }
 
 
-    
-    /** Si le déplacement de l'entité est autorisé (pas de mur ou autre entité), il est réalisé
+    /**
+     * Si le déplacement de l'entité est autorisé (pas de mur ou autre entité), il est réalisé
      * Sinon, rien n'est fait.
      */
-    public boolean moveEntity(Entity e, Direction d)
-    {
+    public boolean moveEntity(Entity e, Direction d) {
         boolean back = true;
-        
+
         Point pointPosition = map.get(e.getTile());
-        
+
         Point pointTarget = calculateTargetPoint(pointPosition, d);
 
-        if (isInGrid(pointTarget))
-        {
-            Entity entityTarget= tilePosition(pointTarget).getEntity();
-            if (entityTarget != null)
-            {
+        if (isInGrid(pointTarget)) {
+            Entity entityTarget = tilePosition(pointTarget).getEntity();
+            if (entityTarget != null) {
                 entityTarget.push(d);
             }
 
             // si la case est libérée
-            if (tilePosition(pointTarget).canMove())
-            {
+            if (tilePosition(pointTarget).canMove()) {
                 e.getTile().leaveTile();
                 tilePosition(pointTarget).goOnTile(e);
 
-            }
-            else
-            {
+            } else {
                 back = false;
             }
 
-        }
-        else
-        {
+        } else {
             back = false;
         }
 
         return back;
     }
-    
-    
-    private Point calculateTargetPoint(Point pointPosition, Direction d)
-    {
+
+
+    private Point calculateTargetPoint(Point pointPosition, Direction d) {
         Point pointTarget = null;
-        
-        switch(d)
-        {
-            case haut: pointTarget = new Point(pointPosition.x, pointPosition.y - 1); break;
-            case bas : pointTarget = new Point(pointPosition.x, pointPosition.y + 1); break;
-            case gauche : pointTarget = new Point(pointPosition.x - 1, pointPosition.y); break;
-            case droite : pointTarget = new Point(pointPosition.x + 1, pointPosition.y); break;
-            
+
+        switch (d) {
+            case haut:
+                pointTarget = new Point(pointPosition.x, pointPosition.y - 1);
+                break;
+            case bas:
+                pointTarget = new Point(pointPosition.x, pointPosition.y + 1);
+                break;
+            case gauche:
+                pointTarget = new Point(pointPosition.x - 1, pointPosition.y);
+                break;
+            case droite:
+                pointTarget = new Point(pointPosition.x + 1, pointPosition.y);
+                break;
+
         }
-        
+
         return pointTarget;
     }
-    
 
-    
-    /** Indique si p est contenu dans la grille
+
+    /**
+     * Indique si p est contenu dans la grille
      */
     private boolean isInGrid(Point p) {
         return p.x >= 0 && p.x < SIZE_X && p.y >= 0 && p.y < SIZE_Y;
     }
-    
-    private Tile tilePosition(Point p)
-    {
+
+    private Tile tilePosition(Point p) {
         Tile back = null;
-        
-        if (isInGrid(p))
-        {
+
+        if (isInGrid(p)) {
             back = entityGrid[p.x][p.y];
         }
-        
+
         return back;
     }
 
+    public String tileTypeString(Tile t) {
+        if (t.getEntity() instanceof Block) {
+            return "B";
+        } else if (t.getEntity() instanceof Hero) {
+            return "H";
+        } else if (t instanceof Empty) {
+            return "_";
+        } else if (t instanceof Wall) {
+            return "W";
+        } else {
+            return " ";
+        }
+    }
+
+    public void writeMapToFile(String fileName) {
+        int n_hero = 0;
+        Tile heroTile = hero.getTile(); // Obtenez le Tile associé au héros
+        Point heroPosition = map.get(heroTile); // Obtenez la position de ce Tile à partir de la HashMap
+        //System.out.println(hero.getTile().getEntity().getClass().getName());
+        try {
+            PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+            writer.println(SIZE_X);
+            writer.println(SIZE_Y);
+            for (int i = 0; i < SIZE_Y; i++) {
+                for (int j = 0; j < SIZE_X; j++) {
+                    writer.print(tileTypeString(entityGrid[j][i]) + ",");
+                }
+                writer.println();
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads a map from a file and updates the entityGrid and hero accordingly
+     *
+     * @param fileName the name of the file to read from
+     */
+    public void readMapFromFile(String fileName) {
+        int n_hero = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            int x = Integer.parseInt(reader.readLine());
+            int y = Integer.parseInt(reader.readLine());
+            Tile[][] entityGrid = new Tile[x][y];
+            for (int i = 0; i < y; i++) {
+                String line = reader.readLine();
+                String[] tiles = line.split(",");
+                for (int j = 0; j < x; j++) {
+                    switch (tiles[j]) {
+                        case "_":
+                            addTile(new Empty(this), j, i);
+                            break;
+                        case "W":
+                            addTile(new Wall(this), j, i);
+                            break;
+                        case "B":
+                            addTile(new Empty(this), j, i);
+                            entityGrid[j][i].setEntity(new Block(this, entityGrid[j][i]));
+                            break;
+                        case "H":
+                            addTile(new Empty(this), j, i);
+                            hero = new Hero(this, entityGrid[j][i]);
+                            entityGrid[j][i].setEntity(hero);
+                            n_hero++;
+                            break;
+                        default:
+                           addTile(new Empty(this), j, i);
+                            break;
+                    }
+                }
+            }
+            reader.close();
+            for (int i = 0; i < SIZE_Y; i++) {
+                for (int j = 0; j < SIZE_X; j++) {
+                    System.out.print(tileTypeString(entityGrid[j][i]) + ",");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading from the file.");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /*
+    public void test(String fileName) {
+        try {
+
+            System.out.println("debut_test");
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            int x = Integer.parseInt(reader.readLine());
+            int y = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < y; i++) {
+                String line = reader.readLine();
+                String[] tiles = line.split(",");
+                for (int j = 0; j < x; j++) {
+                    System.out.print(tiles[j] + "|");
+                }
+                System.out.println();
+            }
+            reader.close();
+            System.out.println("fin_test");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading from the file.");
+            e.printStackTrace();
+        }
+    }
+    */
 }
